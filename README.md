654682cdc6bacef6d0ed4cef378856f251cd16f2
Anastasia <nastia.iv.91@gmail.com>
2020-10-17 19:49:04 -0700

    script file and readMe finalized. links and credits added

--- README.md
+++ README.md
@@ -17,12 +17,33 @@ For this task we needed to unstall the VS Code programm and use Google Chrome to
 
 ## Usage
 
-With the world wide web growing exponentially, we need to make sure that our private data is safe from hackers, who's numbers are also have been growing at a fast pace. Due to this, it's very important to not just have some custom password protecting your devices, but to have a strong reliable one, so the malware couldn't get through.
-To fullfill these requirements, we have created an easy to use, functional application that randomly picks characters according to clients choice.
-First, we have created a file structure with the index page, that contains an epmty field for the password, that will be generated after the client will click on the button "Generate Pasword". Then he will be presented with a question about the length of the password. If the client decides to cancel the execution at that moment, the dialog window will dissapear and he will be able to start over. After indicating the criterias that he wants to add, the client receives a password. Once the functions in the file have had been executed, the application will retrieve a series of random numbers and characters from the ASCII code.
+With the world wide web growing exponentially, we need to make sure that our private data is safe from hackers, who's numbers are also have been growing at a fast pace. Due to this, it's very important to not just have some custom password protecting your devices, but to have a strong reliable one, so the malware couldn't get through.<br>
+To fullfill these requirements, we have created an easy to use, functional application that randomly picks characters according to clients choice.<br>
+First, we have created a file structure with the index page, that contains an epmty field for the password, that will be generated after the client will click on the button "Generate Pasword". Then he will be presented with a question about the length of the password. If the client decides to cancel the execution at that moment, the dialog window will dissapear and he will be able to start over. <br>
+After indicating the criterias that he wants to add, the client receives a password. Once the functions in the file have had been executed, the application will retrieve a series of random numbers and characters from the ASCII code, that would be very difficult to hack.
 
 
-Attached below screenshots of the working applicaton, that is deployed at live URL and loads with no errors.
-First dialog window:
-[First dialog window](screenshots/211.png)
+Attached below the screenshots of the working applicaton, that is deployed at live URL and loads with no errors.
+First dialog window: 
+[First dialog window](screenshots/211.jpg)
+
+The client chooses what types of characters he wants in his password:
+[Second dialog window](screenshots/214.jpg)
+
+The password is generated and ready to be copied.
+[First dialog window](screenshots/213.jpg)
+
+
+## Links
+
+[Password generator](https://anaiva27.github.io/Password-Generator-App/) <br>
+[GitHub repository](https://github.com/anaiva27/Password-Generator-App)
+
+
+## Credits
+
+Useful articles about Javascript functions, statements and loops and implemention of the ASCII codes:<br>
+[Use of charcodes](https://www.w3schools.com/jsref/jsref_fromcharcode.asp)<br>
+[CodeAcademy. Functions](https://www.codecademy.com/learn/introduction-to-javascript/modules/learn-javascript-functions)<br>
+[Javascript Objects and math](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)<br>
 
--- script3.js
+++ script3.js
@@ -3,58 +3,58 @@ var generateBtn = document.querySelector("#generate");
 
 //Password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-passwordText.value = password;};
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 //Variables and arrays to be used
-var UpperCharCodes = arrayCodes(65,90);
-var LowerCharCodes = arrayCodes(97,122);
-// ​var UpperCharCodes = arrayCodes(65,90);
-var NumberCharCodes = arrayCodes(48,57);
-var SpecialCharCodes = arrayCodes(33,47).concat(arrayCodes(58,64)).concat(arrayCodes(91,96)).concat(arrayCodes(123,126));
+var UpperCharCodes = arrayCodes(65, 90);
+var LowerCharCodes = arrayCodes(97, 122);
+var NumberCharCodes = arrayCodes(48, 57);
+var SpecialCharCodes = arrayCodes(33, 47).concat(arrayCodes(58, 64)).concat(arrayCodes(91, 96)).concat(arrayCodes(123, 126));
 
-var choiceUpperCase = ""; 
-var choiceLowerCase = ""; 
-var choiceNum = ""; 
+var choiceUpperCase = "";
+var choiceLowerCase = "";
+var choiceNum = "";
 var choiceSpecialChar = "";
 
 
 // Function for generating a password according to criterias user had picked 
-function generatePassword(){
+function generatePassword() {
     var choiceLength = parseInt(prompt("Enter desired number of characters for Your password between 8 and 128"));
-    if (choiceLength <= 7 || choiceLength >= 129 || isNaN(choiceLength))
-{    alert("Please choose a number between 8 and 128")
-}
-  else{
- var choiceUpperCase = confirm("Would you like to add upper case letters? Yes or No "); 
- var choiceLowerCase = confirm("Would you like to add lower case letters? Yes or No ");
- var choiceNum = confirm("Would you like to add numbers? Yes or No ");
- var choiceSpecialChar = confirm("Would you like to add special characters? Yes or No ");
+    if (choiceLength <= 7 || choiceLength >= 129 || isNaN(choiceLength)) {
+        alert("Please choose a number between 8 and 128");
+    }
+    else {
+        var choiceUpperCase = confirm("Would you like to add upper case letters? Yes or No ");
+        var choiceLowerCase = confirm("Would you like to add lower case letters? Yes or No ");
+        var choiceNum = confirm("Would you like to add numbers? Yes or No ");
+        var choiceSpecialChar = confirm("Would you like to add special characters? Yes or No ");
 
-}
+    }
 
-// Creating array of all character codes to randomly choose one each time 
-    var charCodes=[];
-    if (choiceSpecialChar) charCodes = charCodes.concat(SpecialCharCodes);  
+    // Creating array of all character codes to randomly choose one each time 
+    var charCodes = [];
+    if (choiceSpecialChar) charCodes = charCodes.concat(SpecialCharCodes);
     if (choiceLowerCase) charCodes = charCodes.concat(LowerCharCodes);
     if (choiceUpperCase) charCodes = charCodes.concat(UpperCharCodes);
     if (choiceNum) charCodes = charCodes.concat(NumberCharCodes);
-    var passwordCharacter=[];
-    for (let i=0; i < choiceLength; i++){
-        var characterCode = charCodes[Math.floor(Math.random()*charCodes.length)];
+    var passwordCharacter = [];
+    for (let i = 0; i < choiceLength; i++) {
+        var characterCode = charCodes[Math.floor(Math.random() * charCodes.length)];
         passwordCharacter.push(String.fromCharCode(characterCode));
     }
     return passwordCharacter.join("");
 }
 
 // Generating array from ASCII codes
-function arrayCodes(min,max){
+function arrayCodes(min, max) {
     var array = [];
-    for ( let i = min; i <= max; i++){
+    for (let i = min; i <= max; i++) {
         array.push(i);
     }
     return array;
